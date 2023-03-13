0604d5ce702c81f593ec1395437ef49d365ca8aa
glaubercasttro <glaubercasttro@gmail.com>
2023-03-13 11:49:16 -0300

    Iniciando teste com kafka basicos

---
+++ Projetos/Kafka/Consumidor/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using cursokafka;
+
+//Servidor do Schema Regsitry
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+//classe de cnfiguração para não precisar bater sempre no servidor do schema registry aumentando o desempenho da aplicação
+var schemaregistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ConsumerConfig
+{
+    GroupId = "devIo",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, curso>(config)
+    .SetValueDeserializer(new AvroDeserializer<curso>(schemaregistry)
+    .AsSyncOverAsync()).Build();
+
+consumer.Subscribe("Cursos");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"{result.Message.Key} - {result.Message.Value.Descricao}");
+}
---
+++ Projetos/Kafka/Produtor/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using cursokafka;
+
+//Servidor do Schema Regsitry
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+//classe de cnfiguração para não precisar bater sempre no servidor do schema registry aumentando o desempenho da aplicação
+var schemaregistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+//Produz uma mensagem do kafka do tipo curso
+using var producer = new ProducerBuilder<string, curso>(config)
+    .SetValueSerializer(new AvroSerializer<curso>(schemaregistry))
+    .Build();
+
+var message = new Message<string, curso>()
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new curso
+    {
+        Id = Guid.NewGuid().ToString(),
+        Descricao = "Primeiro objeto avro no kafka"
+    }
+};
+
+var result = await producer.ProduceAsync("Cursos", message);
+
+Console.WriteLine($"{result.Offset} - {result.TopicPartition}");
+Console.ReadKey();
